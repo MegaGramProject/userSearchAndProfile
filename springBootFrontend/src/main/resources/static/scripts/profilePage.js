ed221237693f6285c49d009cc1a2d51495fcea4d
Rishav Ray <risjavram@gmail.com>
2024-09-22 00:19:22 -0500

    made very significant strides in frontend of user-profile main-section

--- springBootFrontend/src/main/resources/static/scripts/profilePage.js
+++ springBootFrontend/src/main/resources/static/scripts/profilePage.js
@@ -1,7 +1,33 @@
 const leftSidebarPopup = document.getElementById('leftSidebarPopup');
+const showSimilarAccountsIcon = document.getElementById('showSimilarAccountsIcon');
+const showSimilarAccountsIcon2 = document.getElementById('showSimilarAccountsIcon2');
+const followButton = document.getElementById('followButton');
+const followingButton = document.getElementById('followingButton');
+const postsGridIcon = document.getElementById('postsGridIcon');
+const postsGridIcon2 = document.getElementById('postsGridIcon2');
+const postsText = document.getElementById('postsText');
+const reelsIcon = document.getElementById('reelsIcon');
+const reelsIcon2 = document.getElementById('reelsIcon2');
+const reelsText = document.getElementById('reelsText');
+const taggedPostsIcon = document.getElementById('taggedPostsIcon');
+const taggedPostsIcon2 = document.getElementById('taggedPostsIcon2');
+const taggedText = document.getElementById('taggedText');
+const suggestedAccounts = document.getElementById('suggestedAccounts');
+const listOfFollowersPopup = document.getElementById('listOfFollowersPopup');
+const mainSection = document.getElementById('mainSection');
+const listOfFollowingsPopup = document.getElementById('listOfFollowingsPopup');
+const listOfMutualFollowersPopup = document.getElementById('listOfMutualFollowersPopup');
+const optionsPopup = document.getElementById('optionsPopup');
 
 let authenticatedUsername = "";
 let displayLeftSidebarPopup = false;
+let displaySimilarAccounts = false;
+let isFollowingUser = false;
+let currentlyShownSection = "POSTS";
+let displayListOfFollowersPopup = false;
+let displayListOfFollowingsPopup = false;
+let displayListOfMutualFollowersPopup = false;
+
 
 async function authenticateUser() {
     const username = document.getElementById('authenticatedUsername').textContent;
@@ -124,5 +150,231 @@ function takeUserToOwnProfile() {
 function takeUserToLogin() {
     window.location.href = "http://localhost:8000/login";
 }
+
+function toggleSimilarAccounts() {
+    displaySimilarAccounts = !displaySimilarAccounts;
+    if(displaySimilarAccounts) {
+        showSimilarAccountsIcon.classList.add('hidden');
+        showSimilarAccountsIcon2.classList.remove('hidden');
+        suggestedAccounts.classList.remove('hidden');
+    }
+    else {
+        showSimilarAccountsIcon.classList.remove('hidden');
+        showSimilarAccountsIcon2.classList.add('hidden');
+        suggestedAccounts.classList.add('hidden');
+    }
+}
+
+function toggleFollow() {
+    isFollowingUser = !isFollowingUser;
+    if(isFollowingUser) {
+        followButton.classList.add('hidden');
+        followingButton.classList.remove('hidden');
+    }
+    else {
+        followButton.classList.remove('hidden');
+        followingButton.classList.add('hidden');
+    }
+}
+
+function showReels() {
+    if(currentlyShownSection!=='REELS') {
+        if(currentlyShownSection==='POSTS') {
+            postsGridIcon.classList.remove('hidden');
+            postsGridIcon2.classList.add('hidden');
+            postsText.style['color'] = "";
+            postsText.style['font-size'] = "";
+        }
+        else if(currentlyShownSection==='TAGGED') {
+            taggedPostsIcon.classList.remove('hidden');
+            taggedPostsIcon2.classList.add('hidden');
+            taggedText.style['color'] = "";
+            taggedText.style['font-size'] = "";
+        }
+
+        reelsIcon.classList.add('hidden');
+        reelsIcon2.classList.remove('hidden');
+        reelsText.style['color'] = "black";
+        reelsText.style['font-size'] = "1.15em";
+        currentlyShownSection = 'REELS';
+    }
+    else {
+        //refresh the reels
+    }
+}
+
+function showPosts() {
+    if(currentlyShownSection!=='POSTS') {
+        if(currentlyShownSection==='REELS') {
+            reelsIcon.classList.remove('hidden');
+            reelsIcon2.classList.add('hidden');
+            reelsText.style['color'] = "";
+            reelsText.style['font-size'] = "";
+        }
+        else if(currentlyShownSection==='TAGGED') {
+            taggedPostsIcon.classList.remove('hidden');
+            taggedPostsIcon2.classList.add('hidden');
+            taggedText.style['color'] = "";
+            taggedText.style['font-size'] = "";
+        }
+
+        postsGridIcon.classList.add('hidden');
+        postsGridIcon2.classList.remove('hidden');
+        postsText.style['color'] = "black";
+        postsText.style['font-size'] = "1.15em";
+        currentlyShownSection = 'POSTS';
+    }
+    else {
+        //refresh the posts
+    }
+
+}
+
+function showTaggedPosts() {
+    if(currentlyShownSection!=='TAGGED') {
+        if(currentlyShownSection==='REELS') {
+            reelsIcon.classList.remove('hidden');
+            reelsIcon2.classList.add('hidden');
+            reelsText.style['color'] = "";
+            reelsText.style['font-size'] = "";
+        }
+        else if(currentlyShownSection==='POSTS') {
+            postsGridIcon.classList.remove('hidden');
+            postsGridIcon2.classList.add('hidden');
+            postsText.style['color'] = "";
+            postsText.style['font-size'] = "";
+        }
+
+        taggedPostsIcon.classList.add('hidden');
+        taggedPostsIcon2.classList.remove('hidden');
+        taggedText.style['color'] = "black";
+        taggedText.style['font-size'] = "1.15em";
+        currentlyShownSection = 'TAGGED';
+    }
+    else {
+        //refresh the reels
+    }
+
+}
+
+function closeListOfFollowersPopup() {
+    displayListOfFollowersPopup = false;
+    listOfFollowersPopup.classList.add('hidden');
+    mainSection.style['background-color']= 'white';
+    mainSection.style['pointer-events']= 'auto';
+}
+
+function messageUser() {
+    window.location.href = "http://localhost:8011/directMessaging/"+authenticatedUsername;
+}
+
+function showListOfFollowersPopup() {
+    displayListOfFollowersPopup = true;
+    listOfFollowersPopup.classList.remove('hidden');
+    mainSection.style['background-color'] = 'gray';
+    mainSection.style['pointer-events']= 'none';
+    listOfFollowersPopup.style['pointer-events']= 'auto';
+}
+
+function takeToUsersProfile(username) {
+    window.location.href = "http://localhost:8019/profilePage/"+authenticatedUsername + "/" + username;
+}
+
+function showListOfFollowingsPopup() {
+    displayListOfFollowingsPopup = true;
+    listOfFollowingsPopup.classList.remove('hidden');
+    mainSection.style['background-color'] = 'gray';
+    mainSection.style['pointer-events']= 'none';
+    listOfFollowingsPopup.style['pointer-events']= 'auto';
+}
+
+function closeListOfFollowingsPopup() {
+    displayListOfFollowingsPopup = false;
+    listOfFollowingsPopup.classList.add('hidden');
+    mainSection.style['background-color']= 'white';
+    mainSection.style['pointer-events']= 'auto';
+}
+
+
+function toggleFollowInPopup(popupName, username) {
+    let followButtonToToggle = document.getElementById(popupName+"Follow"+username);
+    let followingButtonToToggle = document.getElementById(popupName+"Following"+username);
+
+    if(followButtonToToggle.classList.contains('hidden')) {
+        followButtonToToggle.classList.remove('hidden');
+        followingButtonToToggle.classList.add('hidden');
+    }
+    else {
+        followButtonToToggle.classList.add('hidden');
+        followingButtonToToggle.classList.remove('hidden');
+    }
+}
+
+function removeSuggestedAccount(username) {
+    const suggestedAccountDivToRemove = document.getElementById('suggestedAccount' + username);
+    suggestedAccountDivToRemove.style['display'] = 'none';
+}
+
+function showListOfMutualFollowersPopup() {
+    displayListOfMutualFollowersPopup = true;
+    listOfMutualFollowersPopup.classList.remove('hidden');
+    mainSection.style['background-color'] = 'gray';
+    mainSection.style['pointer-events']= 'none';
+    listOfMutualFollowersPopup.style['pointer-events']= 'auto';
+}
+
+function closeListOfMutualFollowersPopup() {
+    displayListOfMutualFollowersPopup = false;
+    listOfMutualFollowersPopup.classList.add('hidden');
+    mainSection.style['background-color']= 'white';
+    mainSection.style['pointer-events']= 'auto';
+}
+
+function cancelOptionsPopup() {
+    optionsPopup.classList.add('hidden');
+    mainSection.style['background-color']= 'white';
+    mainSection.style['pointer-events']= 'auto';
+}
+
+
+function showOptionsPopup() {
+    optionsPopup.classList.remove('hidden');
+    mainSection.style['background-color'] = 'gray';
+    mainSection.style['pointer-events']= 'none';
+    optionsPopup.style['pointer-events']= 'auto';
+}
+
+function showNumLikesAndCommentsOfPost(postId) {
+    let DOMElementsToShow =
+    [
+        document.getElementById(postId+'WhiteHeartIcon'),
+        document.getElementById(postId+'NumLikes'),
+        document.getElementById(postId+'WhiteCommentIcon'),
+        document.getElementById(postId+'NumComments'),
+    ];
+    for(let element of DOMElementsToShow) {
+        element.classList.remove('hidden');
+    }
+    let postImage = document.getElementById(postId);
+    postImage.style['opacity'] = '0.85';
+}
+
+
+function hideNumLikesAndCommentsOfPost(postId) {
+    let DOMElementsToHide =
+    [
+        document.getElementById(postId+'WhiteHeartIcon'),
+        document.getElementById(postId+'NumLikes'),
+        document.getElementById(postId+'WhiteCommentIcon'),
+        document.getElementById(postId+'NumComments'),
+    ];
+    for(let element of DOMElementsToHide) {
+        element.classList.add('hidden');
+    }
+    let postImage = document.getElementById(postId);
+    postImage.style['opacity'] = '1';
+}
+
+
 
 authenticateUser();
